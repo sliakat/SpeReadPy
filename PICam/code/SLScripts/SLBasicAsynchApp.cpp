36e3f31a0bb219d528a655e1f4a3c5a7512bcaea
sliakat <76749892+sliakat@users.noreply.github.com>
2021-07-01 12:08:38 -0400

    Update SLBasicAsynchApp.cpp

--- PICam/code/SLScripts/SLBasicAsynchApp.cpp
+++ PICam/code/SLScripts/SLBasicAsynchApp.cpp
@@ -1,4 +1,16 @@
 //Basic example of asynchronous acquisiton with a virtual PI-MAX4.
+//Incoming data stream is displayed using OpenCV.
+//
+//Scope: Demonstrate a simple way to observe, copy, and display incoming
+//  frame data from an aynchronous acquisiton.
+//External library requirement: **MUST have OpenCV installed and properly linked.**
+//  This program was tested with OpenCV 4.5.2
+//Limitations: A virtual PI-MAX4 1024i is hard-coded with some pre-determined parameters,
+//  so that any user can just compile, run, and observe without need for any input.
+//  These parameters may not apply to all cameras. Please keep in mind the scope of the
+//  example is to demonstrate the asynchronous acquisiton. For more advanced usage, please
+//  refer to the PICam samples provided with the SDK.
+
 
 //set number of frames to asynchronously acquire
 #define NUM_FRAMES  100
@@ -7,7 +19,11 @@
 #include <vector>
 #include "picam.h"
 
-//set virtual camera to run at ~25fps
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+//set virtual camera to run at ~25fps (applicable for a virtual PI-MAX 1024i)
 void ArmParameters(PicamHandle cam)
 {
     piflt readRate; const PicamParameter* failed_parameter_array; piint failed_parameter_count = 0;
@@ -49,16 +65,27 @@ void Acquire(PicamHandle cam)
     PicamAcquisitionStatus status; PicamAvailableData available; piint readoutStride; piint frameSize;
     pi64s lastReadoutOffset; pibyte* frame; pi16u* frameptr = NULL;
     PicamError err = PicamError_None;
+
+    //get readout bytes and frame elements
     Picam_GetParameterIntegerValue(cam, PicamParameter_ReadoutStride, &readoutStride);
     Picam_GetParameterIntegerValue(cam, PicamParameter_FrameSize, &frameSize);
     std::vector<pi16u> imageData16(frameSize / 2, 0);
-    //printf("Frame Size: %d\n", frameSize);
+
+    //get height and width of opened camera (to set up image containers)
+    piint rows, cols;
+    Picam_GetParameterIntegerValue(cam, PicamParameter_SensorActiveHeight, &rows);
+    Picam_GetParameterIntegerValue(cam, PicamParameter_SensorActiveWidth, &cols);
+    //openCV image object with the size of camera (rows, cols)
+    cv::Mat image = cv::Mat(rows, cols, CV_16UC1);
+    cv::Mat adjImage; //destination for contrast-adjusted image
+
     Picam_StartAcquisition(cam);
     printf("**Acquisiton Started...\n");
 
     do
     {
         Picam_WaitForAcquisitionUpdate(cam, -1, &available, &status);
+        //image processing and display happens in the following loop
         if (available.readout_count > 0)
         {
             //move pointer to the most recent readout in the circular buffer
@@ -67,12 +94,17 @@ void Acquire(PicamHandle cam)
             frameptr = reinterpret_cast<pi16u*>(frame);
             //copy image data from AvailableData into vector
             //if running with other threads that have PICam access, use mutex lock on initial_readout pointer
-            std::vector<pi16u> imageData16(frameptr, frameptr + frameSize/2);
+            std::vector<pi16u> imageData16(frameptr, frameptr + frameSize / 2);
             printf("\tReadout Received. Image Mean: %0.2f counts.\n", GetMean(imageData16));
+            std::memcpy(image.data, imageData16.data(), imageData16.size() * sizeof(pi16u));
+            cv::normalize(image, adjImage, 0, 65535, cv::NORM_MINMAX);
+            cv::imshow("Frame", adjImage);
+            cv::waitKey(1);
         }
     } while (status.running || err == PicamError_TimeOutOccurred);
     Picam_StopAcquisition(cam);
-    printf("...Acquisiton Stopped**\n");
+    printf("...Acquisiton Stopped**\nPress any key after clicking display, or wait 20 secs to exit image display.\n");
+    cv::waitKey(20000);         //leaves final frame up for 20 secs or key press
     return;
 }
 
