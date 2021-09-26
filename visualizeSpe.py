31d011adcd8d89bd7a3cc927b4c075c48a24ba97
sliakat <76749892+sliakat@users.noreply.github.com>
2021-09-26 18:47:27 -0400

    Update visualizeSpe.py
    now lets you pick multiple spe files from opening dialog

--- visualizeSpe.py
+++ visualizeSpe.py
@@ -17,52 +17,52 @@ root.withdraw()
 
 regionToDisplay=0 #this will display ROI #1
 
-filePath = filedialog.askopenfilename()
-
-totalData = readSpe(filePath)
-waveCal=False
-try:
-    xmlFooter = totalData.xmlFooter
-    wavelengths = totalData.wavelengths
-    waveCal=True
-except:
-    print('No wavelength calibration in spe.')
-
-dataList = totalData.data
-
-#image contrast adjustments
-display_min = np.int(np.percentile(dataList[regionToDisplay].flatten(),5))
-display_max = np.int(np.percentile(dataList[regionToDisplay].flatten(),95))
-
-if display_min < 1:
-    display_min = 1
-
-# if display_max > 65535:
-#     display_max = 65535
-
-figCount=0
-displayRange = range(0,5)
-
-for k in displayRange: #whatever range of figs dictated by displayRange
-    data = dataList[regionToDisplay][k,:,:]
-    fig = plt.figure('Frame %d'%(k+1))
-    ax = fig.add_subplot(111)
-    if np.size(data,0)==1:
-        try:
-            ax.plot(wavelengths,data[0])
-            ax.set_xlabel('Wavelength (nm)')
-        except:
-            ax.plot(data[0])
-        ax.set_ylabel('Intensity (counts)')
-    else:
-        if waveCal==True:
-            #aspect=(np.size(data,0)/np.size(data,1))
-            aspect = 0.25
-            ax.imshow(data,vmin=display_min,vmax=display_max,cmap='gray',extent=[wavelengths[0],wavelengths[-1],np.size(data,0),0],aspect=aspect)
-            ax.set(xlabel='Wavelength (nm)')
+filePath = filedialog.askopenfilenames()    #can select multiple files
+for i in range(0,len(filePath)):
+    totalData = readSpe(filePath[i])
+    waveCal=False
+    try:
+        xmlFooter = totalData.xmlFooter
+        wavelengths = totalData.wavelengths
+        waveCal=True
+    except:
+        print('No wavelength calibration in spe.')
+    
+    dataList = totalData.data
+    
+    #image contrast adjustments
+    display_min = int(np.percentile(dataList[regionToDisplay].flatten(),5))
+    display_max = int(np.percentile(dataList[regionToDisplay].flatten(),95))
+    
+    if display_min < 1:
+        display_min = 1
+    
+    # if display_max > 65535:
+    #     display_max = 65535
+    
+    figCount=0
+    displayRange = range(0,1)
+    
+    for k in displayRange: #whatever range of figs dictated by displayRange
+        data = dataList[regionToDisplay][k,:,:]
+        fig = plt.figure('%s, Frame %d'%(filePath[i],k+1))
+        ax = fig.add_subplot(111)
+        if np.size(data,0)==1:
+            try:
+                ax.plot(wavelengths,data[0])
+                ax.set_xlabel('Wavelength (nm)')
+            except:
+                ax.plot(data[0])
+            ax.set_ylabel('Intensity (counts)')
         else:
-            ax.imshow(data,origin='upper',vmin=display_min,vmax=display_max,cmap='gray')
-            ax.set(xlabel='Column')
-        ax.set(ylabel='Row')
-        if k+1 >= np.size(dataList[regionToDisplay],0):
-            break
+            if waveCal==True:
+                #aspect=(np.size(data,0)/np.size(data,1))
+                aspect = 0.05
+                ax.imshow(data,vmin=display_min,vmax=display_max,cmap='gray',extent=[wavelengths[0],wavelengths[-1],np.size(data,0),0],aspect=aspect)
+                ax.set(xlabel='Wavelength (nm)')
+            else:
+                ax.imshow(data,origin='upper',vmin=display_min,vmax=display_max,cmap='gray')
+                ax.set(xlabel='Column')
+            ax.set(ylabel='Row')
+            if k+1 >= np.size(dataList[regionToDisplay],0):
+                break
