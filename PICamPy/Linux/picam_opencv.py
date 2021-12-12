3c8263326dde4a7ef6c9d829b174b5303c8eaeca
sliakat <76749892+sliakat@users.noreply.github.com>
2021-12-11 19:27:42 -0500

    added line to test the preview with callback

--- PICamPy/Linux/picam_opencv.py
+++ PICamPy/Linux/picam_opencv.py
@@ -5,8 +5,12 @@ import picam
 import numpy as np
 
 cam = picam.Camera()
-cam.OpenFirstCamera(model=60)   #will open live camera if connected, otherwise opens virtual demo cam
-data = cam.Acquire(frames=50)   #when acquire finishes, pressing any key or waiting 20s will exit the opencv viewer
+cam.OpenFirstCamera(model=1206)   #will open live camera if connected, otherwise opens virtual demo cam
+#data = cam.Acquire(frames=50)   #when acquire finishes, pressing any key or waiting 20s will exit the opencv viewer
+cam.AcquireCB(frames=100)       #asynchronously acquire with callback and preview n frames w/ opencv
 cam.Close()
 
-print(np.shape(data)) #total acquired data will be available as numpy array for further analysys
+try:
+    print(np.mean(data[-1,:,:])) #total acquired data will be available as numpy array for further analysis
+except:
+    pass
