3c6a97fd0d5506af01c675018e6e9508894e46d1
sliakat <76749892+sliakat@users.noreply.github.com>
2021-12-17 14:25:01 -0500

    adjusted to test asynch AcquireCB

--- PICamPy/Linux/picam_opencv.py
+++ PICamPy/Linux/picam_opencv.py
@@ -6,8 +6,13 @@ import numpy as np
 
 cam = picam.Camera()
 cam.OpenFirstCamera(model=1206)   #will open live camera if connected, otherwise opens virtual demo cam
-#data = cam.Acquire(frames=50)   #when acquire finishes, pressing any key or waiting 20s will exit the opencv viewer
-cam.AcquireCB(frames=100)       #asynchronously acquire with callback and preview n frames w/ opencv
+asynch = True
+if asynch:
+    cam.AcquireCB(frames=100)       #asynchronously acquire with callback and preview n frames w/ opencv
+    cam.DisplayCameraData()
+else:
+    data = cam.Acquire(frames=50)   #when acquire finishes, pressing any key or waiting 20s will exit the opencv viewer
+
 cam.Close()
 
 try:
