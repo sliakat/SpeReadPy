c489f84f96a05bf395f2388c478fd0cd168dfedf
sliakat <76749892+sliakat@users.noreply.github.com>
2022-01-03 13:23:50 -0500

    Update picam_opencv.py

--- PICamPy/Linux/picam_opencv.py
+++ PICamPy/Linux/picam_opencv.py
@@ -5,7 +5,7 @@ import picam
 import numpy as np
 import time
 
-cam = picam.Camera()
+cam = picam.Camera()    #default behavior is to look in Linux path. To use with Windows, enter libPath='<<path to Picam.dll>>'
 cam.OpenFirstCamera(model=1206)   #will open live camera if connected, otherwise opens virtual demo cam
 cam.SetExposure(50)     #exposure time in ms
 asynch = True      #toggle to check the different acquisiton functions
