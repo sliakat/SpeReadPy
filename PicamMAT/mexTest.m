6ff0d960c8b1be010e83b29151e8af30a733df91
sliakat <sabbir.liakat@teledyne.com>
2023-11-17 17:32:15 -0500

    added n center bin, helpers for debug printing

--- PicamMAT/mexTest.m
+++ PicamMAT/mexTest.m
@@ -7,16 +7,15 @@
 %any numerical second input (i.e. picamMex(0,1)) activates logging mode,
 %   which appends onto 'mexOutputStrings.txt'
 %
-%the Acquire call now will look for a third parameter (if exists) of
-%   type double. If valid, the user can set the exposure time of that
-%   acquisition using this input.
+%the Acquire call now will look for extra parameters (if exists). See 
+%   documentation in picamMex.cpp
 
-numLoops = 5;  %start with a small number to make sure the program is working the way you expect
+numLoops = 2;  %start with a small number to make sure the program is working the way you expect
                 
 errOpen = picamMex(0, 1);
 if errOpen==0
     for i = 1:numLoops
-        [errAcq,outData] = picamMex(1, 1, i*100);
+        [errAcq,outData] = picamMex(1, 1, i*1000, 1, 50);
         fprintf('Loop number %d, Acquisition error %d\n',i,errAcq);
         %add code here to perform operations before the next acquire
     end
